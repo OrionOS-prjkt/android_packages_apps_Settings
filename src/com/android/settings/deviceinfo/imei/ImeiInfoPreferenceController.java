949a08746643d5ee0ce01a463772accab4dc9c94
romiyusna <yusromi04@gmail.com>
2024-07-12 11:08:43 +0000

    Settings: Redesign About Phone Section
    Design from AkihabaraOS, and some improvement.
    
    All codes mostly from @RisingTechOSS, @DroidX-UI, @xdroid-oss, and stackoverflow website.
    
    Signed-off-by: Iverz <iverzasterix@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: ifloraxd <ifloraxd@gmail.com>

--- src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
@@ -103,6 +103,7 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
             multiImeiPreference.setKey(DEFAULT_KEY + (1 + simSlotNumber));
             multiImeiPreference.setEnabled(true);
             multiImeiPreference.setCopyingEnabled(true);
+            multiImeiPreference.setLayoutResource(R.layout.card_preference_middle);
             category.addPreference(multiImeiPreference);
        }
     }
